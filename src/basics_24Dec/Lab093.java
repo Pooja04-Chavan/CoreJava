388681ab143b48f0e8e00981fe719c7aad6f85d6
Pooja Chavan <poojachavan1597@gmail.com>
2023-12-29 11:06:51 +0530

    24 Dec 2023

---
+++ src/basics_24Dec/Lab093.java
@@ -0,0 +1,15 @@
+package src.basics_24Dec;
+
+public class Lab093 {
+    public static void main(String[] args) {
+
+        //
+        int [] a = {1,2,3};
+        System.out.println(a[10]);
+        // Name of Exception - > java.lang.ArrayIndexOutOfBoundsException
+        // JVM - Giving
+
+
+    }
+
+}
