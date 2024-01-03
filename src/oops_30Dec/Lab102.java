3f4d727d3335bcd5b2f152326293efba24f76060
Pooja Chavan <poojachavan1597@gmail.com>
2024-01-03 12:22:38 +0530

    Oops concepts

---
+++ src/oops_30Dec/Lab102.java
@@ -0,0 +1,12 @@
+package src.oops_30Dec;
+
+public class Lab102 {
+    public static void main(String[] pramod){
+        print("Hello");
+        print("Hi this is python like");
+    }
+
+    static void print(String s){
+        System.out.println(s);
+    }
+}
